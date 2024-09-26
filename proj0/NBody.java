48664d62aa69bfe8add0c54a32dc0e8fa671e895
liangli <liliang11112222@gmail.com>
2024-09-25 17:07:59 -0700

    completed proj0

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -69,7 +69,7 @@ public class NBody {
 
 		/**Print out the universe*/
 		StdOut.printf("%d\n", planets.length);
-		StdOut.printf("%.2e\n", radius);
+		StdOut.printf("%.2e\n", Radius);
 		for (int i = 0; i < planets.length; i++) {
 		    StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
                   	planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
